6347a5a7102a838bbd7175a2a33f82998914877f
marlon nino <mvnino.halcyondigital@gmail.com>
2024-04-14 22:20:29 +0800

    feat: update with the components involved

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
   <p>Choose the currency and the amounts to get the exchange rate</p>
 
   <div class="container">
-    <CurrencyList />
+    <CurrencyList :isReadOnly=false :defaultCurrency="firstCurrency"
+      @selectedCurrencyChanged="handleFirstCurrencySelected" @amountChanged="handleAmountChange" />
 
-    <SwapCurrency />
+    <SwapCurrency :amountToConvert="+amountToConvert" :firstCurrency="firstCurrency" :secondCurrency="secondCurrency" />
 
-    <CurrencyList />
+    <CurrencyList :isReadOnly=true :defaultCurrency="secondCurrency"
+      @selectedCurrencyChanged="handleSecondCurrencySelected" />
   </div>
 </template>
 
@@ -15,4 +17,22 @@
 import Header from "./components/Header.vue"
 import CurrencyList from "./components/CurrencyList.vue"
 import SwapCurrency from "./components/SwapCurrency.vue"
+import { ref } from "vue";
+
+const amountToConvert = ref(0)
+const firstCurrency = ref('USD')
+const secondCurrency = ref('EUR')
+
+const handleAmountChange = (amount) => {
+  amountToConvert.value = amount.value
+}
+
+const handleFirstCurrencySelected = (currencySelected) => {
+  firstCurrency.value = currencySelected.value
+}
+
+const handleSecondCurrencySelected = (currencySelected) => {
+  secondCurrency.value = currencySelected.value
+}
+
 </script>
--- src/components/SwapCurrency.vue
+++ src/components/SwapCurrency.vue
@@ -3,6 +3,37 @@
         <button class="btn" id="swap">
             Swap
         </button>
-        <div class="rate" id="rate"></div>
+        <div class="rate" id="rate">{{ equivalentAmount }} --- {{ amountToConvert }}</div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    amountToConvert: {
+        type: Number,
+        required: true,
+    },
+    firstCurrency: {
+        type: String,
+        required: true,
+    },
+    secondCurrency: {
+        type: String,
+        required: true,
+    }
+})
+
+const equivalentAmount = ref('')
+
+fetch("https://open.exchangerate-api.com/v6/latest")
+    .then(res => res.json())
+    .then(data => {
+        const rate = data.rates[props.secondCurrency] / data.rates[props.firstCurrency];
+        equivalentAmount.value = `${props.amountToConvert} ${props.firstCurrency} = ${rate.toFixed(2)} ${props.secondCurrency}`;
+        
+        console.log('equiii : ', equivalentAmount.value)
+        // amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);/
+    });
+</script>
